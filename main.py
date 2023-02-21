310e87fd89141ed61a2a8fb6c2d5b19102e0c1fa
Gambhir Dhruv <75164918+dhruv-gambhir@users.noreply.github.com>
2023-02-21 17:56:27 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+import yuz
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('gui.ui', self)
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = MyWindow()
+    app.exec_()
